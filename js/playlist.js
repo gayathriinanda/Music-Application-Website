0eea097206843aaf61e1e0bf7f7586f6d1914492
Gayathri Nanda P T <134200322+gayathriinanda@users.noreply.github.com>
2024-06-28 21:54:12 +0530

    Add files via upload

---
+++ js/playlist.js
@@ -0,0 +1,39 @@
+let songs = [
+  
+  {
+    name: "Heat Waves",
+    artist: "Diljit Dosanjh",
+    img: "img02",
+    coverImg: "img02",
+    audio: "Animals"
+  },
+  {
+    name: "Calm Down",
+    artist: "Rema",
+    img: "img01",
+    coverImg: "img01",
+    audio: "calmdown"
+  },
+  {
+    name: "Infinity",
+    artist: "Soulmate",
+    img: "img03",
+    coverImg: "img03",
+    audio: "Infinity"
+  },
+  {
+    name: "Until I Found You",
+    artist: "Stephen Sanchez",
+    img: "img04",
+    coverImg: "img04",
+    audio: "until"
+  }
+  ,
+  {
+    name: "ORePiya",
+    artist: "Arjit Singh",
+    img: "img05",
+    coverImg: "img05",
+    audio: "ORePiya"
+  }
+]
